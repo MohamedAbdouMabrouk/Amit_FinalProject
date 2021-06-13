69d1859e808b2d31f808c611231686c9d01abb91
Mohamed Abdou <73149775+MohamedAbdouMabrouk@users.noreply.github.com>
2021-06-13 18:29:19 +0200

    Update main.c

--- Master/FinalProject.X/main.c
+++ Master/FinalProject.X/main.c
@@ -7,15 +7,19 @@
 
 ISR(USART_RXC_vect)
 {
-    char data = UDR;
+    char data = UDR; //receive data from UART(blutooth or terminal)
     _delay_us(10);  
+    // send data to the another device
     SPI_TREANSMIT(data);  
     
 }
 
 int main() {
+    //initiate SPI
     SPI_INIT(master, SPI_PS_128);
+    //initiate UART
     UART_init(9600);
+    //initiate the global interrupt
     sei();
     while (1) {
     }
