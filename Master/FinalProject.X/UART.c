ffe9d61919ab73eca17a50d8f47fcb3e44fce824
Mohamed Abdou <73149775+MohamedAbdouMabrouk@users.noreply.github.com>
2021-06-13 18:21:02 +0200

    Update UART.c

--- Master/FinalProject.X/UART.c
+++ Master/FinalProject.X/UART.c
@@ -5,25 +5,30 @@
 #include "UART.h"
     
 
-void UART_init(int BaudRate){
+void UART_init(int BaudRate){\
+    // Enable TXEN, RXEN
     UCSRB |= (1<<TXEN)|(1<<RXEN);
+    // Enable RX Interrupt
     UCSRB |= (1<<RXCIE);
+    //set Baud Rate
     UART_setBaudRate(BaudRate);
 }
 
 void UART_setBaudRate(int BaudRate){
-  
+    //calculate baud rate
     int UBRR = ((F_CPU/16.0)/BaudRate)-1;
     UBRRL = (char) UBRR;
     UBRRH = (UBRR>>8);
 }
 void UART_transmit(char data){
+    //wait for data
     while(!(UCSRA & (1<< UDRE)));
     UDR = data;
 }
 
     
 char UART_receive(){
+    //wait for data
     while(!(UCSRA & (1<< RXC)));
     return UDR;
 }
